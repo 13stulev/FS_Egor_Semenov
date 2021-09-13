6d7836ae7471dbd578c630c38abe2907a3b21591
Егор <somaisnotacoma@gmail.com>
2021-09-13 09:40:50 +0400

    Переименовал директории и разобрался с гетерами

--- OOP in Javascript/js/script.js
+++ OOP in Javascript/js/script.js
@@ -4,28 +4,36 @@ class Library {
     get name() {
         return this._name;
     }
+
     set name(value) {
-        if(value != null) {
+        if (value != null) {
             this._name = value;
         }
     }
+
     get books() {
         return this._books;
     }
+
     set books(book) {
         if ((book.id == null) && (book instanceof Book)) {
-            book.id = this._books[Books.length].id + 1;
             this._books.push(book);
+            book.id = this.bookId.id + 1;
+            this.bookId++;
             console.log(book._name + " Была добавлена в библиотеку.");
+        } else if (this._books === undefined) {
+            this._books = [];
         } else {
-            console.log("Это не похоже на книгу.")
+            console.log("Это не похоже на книгу");
         }
     }
 
-    constructor(name, books) {
-        this._name = name;
-        this._books = books
+
+    constructor(name) {
+        this.name = name;
+        this.books = [];
         this.visitorId = 24600;
+        this.bookId = 1;
     }
 
     setReturnal() {
@@ -42,7 +50,7 @@ class Library {
 
         this._books.forEach(e => {
             if ((e._name === bookName) && (e.reservedBy == null)) {
-                visitor.Books.push(e);
+                visitor.books.push(e);
                 this._books[this._books.indexOf(e)].reservedBy = visitor.fullName;
                 this._books[this._books.indexOf(e)].dateOfReturnal = this.setReturnal();
                 console.log(visitor.fullName + " вы забрали " + bookName);
@@ -60,10 +68,11 @@ class Genre {
     }
 
     set type(value) {
-        if(value != null) {
+        if (value != null) {
             this._type = value;
         }
     }
+
     set description(value) {
         this._description = value;
     }
@@ -77,22 +86,24 @@ class Genre {
     }
 
     constructor(type, description) {
-        this._type = type;
-        this._description = description;
+        this.type = type;
+        this.description = description;
     }
 }
 
 class Composition extends Genre {
     set name(value) {
-        if(value != null) {
+        if (value != null) {
             this._name = value;
         }
     }
+
     set author(value) {
-        if(value != null) {
+        if (value != null) {
             this._author = value;
         }
     }
+
     get name() {
         return this._name;
     }
@@ -103,20 +114,20 @@ class Composition extends Genre {
 
     constructor(author, type, description, name) {
         super(type, description)
-        this._author = author;
-        this._name = name;
+        this.author = author;
+        this.name = name;
     }
 }
 
 class Book extends Composition {
     set dateOfEdition(date) {
-        if (date.test(dateRegExp))
-            this._dateOfEdition = value;
+        if (dateRegExp.test(date))
+            this._dateOfEdition = date;
     }
 
     constructor(Composition, dateOfEdition, id, reservedBy, dateOfReturnal) {
         super(Composition.author, Composition.type, Composition.Genre, Composition.name)
-        this._dateOfEdition = dateOfEdition;
+        this.dateOfEdition = dateOfEdition;
         this.id = id;
         this.reservedBy = reservedBy;
         this.dateOfReturnal = dateOfReturnal;
@@ -125,46 +136,55 @@ class Book extends Composition {
 
 class Visitor {
     get Books() {
-        return this._Books;
+        return this._books;
     }
 
     set Books(value) {
-        if( value instanceof Book) {
-            this._Books.push(value);
+        if (value instanceof Book) {
+            this._books.push(value);
+        } else if (this._books === undefined) {
+            this._books = [];
         }
     }
+
     get fullName() {
         return this._fullName;
     }
 
     set fullName(value) {
-        if(value != null) {
+        if (value != null) {
             this._fullName = value;
         }
     }
-    constructor(fullName, address, id, Books) {
-        this._fullName = fullName;
+
+    constructor(fullName, address, id, books) {
+        this.fullName = fullName;
         this.address = address;
         this.id = id;
-        this._Books = [Books];
+        this.books = books;
     }
 }
 
-let jean = new Visitor("Jean Valjean", "France", null, null);
-let mary = new Visitor("Mary Poppins", "London", null, null);
+let jean = new Visitor("Jean Valjean", "France", null, []);
+let mary = new Visitor("Mary Poppins", "London", null, []);
 let visitors = [jean, mary];
 console.log(visitors);
+
 
 let warAndPeace = new Composition("Leo Tolstoy", "novel", null, "War and Peace");
 let lesMiserables = new Composition("Victor Hugo", "tragedy", null, "Les Misérables");
 let fahrenheit = new Composition("Ray Bradbury", "dystopian", null, "451 Fahrenheit");
 
-let firstBook = new Book(warAndPeace, "18-02-2000", 1, null, null);
-let secondBook = new Book(lesMiserables, "18-02-2000", 2, null, null);
-let thirdBook = new Book(fahrenheit, "18-02-2000", 23, null, null);
+let firstBook = new Book(warAndPeace, "18-02-2000", null, null, null);
+let secondBook = new Book(lesMiserables, "18-02-2000", null, null, null);
+let thirdBook = new Book(fahrenheit, "18-02-2000", null, null, null);
 let books = [firstBook, secondBook, thirdBook];
 
-let britishLibrary = new Library("British Library", books);
+let britishLibrary = new Library("British Library");
+britishLibrary.books = firstBook;
+britishLibrary.books = secondBook;
+britishLibrary.books = thirdBook;
+
 britishLibrary.giveABook(jean, "Les Misérables");
 britishLibrary.giveABook(mary, "Les Misérables");
 console.log(britishLibrary);
