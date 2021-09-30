615e3337e2c8bb270a8a4777c759320e8a14f165
Егор <somaisnotacoma@gmail.com>
2021-09-30 22:17:51 +0400

    Выполнил задание

--- JS-и-сеть-Promise-Observable-RxJS/task-2/index.js
+++
@@ -1,9 +0,0 @@
-import { sample1, sample2,} from "./src/rxjs_creation";
-
-// Place for coding
-console.log("Samples");
-
-sample1();
-//sample2();
-//sample3(document.getElementsByTagName("button"));
-//sample4();
